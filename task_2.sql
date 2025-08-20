7c9b97fd8cab36ffa672f9a57d1db7d6bd6ce04a
melatfekadu-s <ejetamelat@gmail.com>
2025-08-20 15:04:59 +0300

    my_python_db

--- task_2.sql
+++ task_2.sql
@@ -22,9 +22,8 @@ CREATE TABLE Books (
 
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
     phone VARCHAR(20),
     address TEXT
 );
