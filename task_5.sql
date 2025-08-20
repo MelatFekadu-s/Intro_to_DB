9c88d8d6a64eca3215db6a54143c0fad8f80e9c7
melatfekadu-s <ejetamelat@gmail.com>
2025-08-20 14:51:31 +0300

    my_python_db

--- task_5.sql
+++ task_5.sql
@@ -2,15 +2,14 @@
 
 INSERT INTO customers (
     customer_id,
-    first_name,
-    last_name,
+    customer_name,
     email,
     address
 )
 VALUES (
     1,
-    'Cole',
-    'Baidoo',
+    'Cole Baidoo',
     'cbaidoo@sandtech.com',
     '123 Happiness Ave.'
 );
+
